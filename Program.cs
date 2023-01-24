313f8c77ba4c43a1350144868eab1e754147ac43
Ya <Ya@DESKTOP-44PBKHO>
2023-01-24 18:03:32 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string b = "start";
+        double previosValue = 0;
+        while (b != "q")
+        {
+            b = Console.ReadLine();
+            bool isDouble = double.TryParse(b, out double numDouble);
+            bool isInt = int.TryParse(b, out int numInt);
+            if (isInt)
+            {
+                Console.WriteLine((char)numInt);
+                previosValue = numInt;
+            }
+            else if (isDouble)
+            {
+                if (numDouble == previosValue) break;
+                previosValue = numDouble;
+
+            }
+        }
+
+    }
+}
